2a5d63ce267cdf52326c4d366c57cc0278957335
Shanazar <98213212+ShanazarDev@users.noreply.github.com>
2024-07-20 22:56:00 +0500

    Update headbot_data.py

--- headbot_data.py
+++ headbot_data.py
@@ -1,6 +1,5 @@
 import requests
 
-
 def get_settings() -> dict[str]:
     settings_url = 'http://192.168.0.252:8045/settings/'
     try:
@@ -8,7 +7,6 @@ def get_settings() -> dict[str]:
         return req.json()
     except Exception:
         return
-
 
 def send_stat(stat_type: str):
     stat_url = 'http://192.168.0.252:8045/bot-stats/'
